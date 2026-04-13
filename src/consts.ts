7abd8a56ba4151dfca9e062c0b3ca27071510ab6
DanyZack <poloniodany@gmail.com>
2026-04-13 23:48:07 +0200

    Catégorie + correction liens articles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'TuneInCloud';
+export const SITE_DESCRIPTION = 'Blog Expertise par Daniel Polònio — Actualités, guides et tutoriels en français.';
